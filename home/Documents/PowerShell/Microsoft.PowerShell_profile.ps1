4000174d07972481ab27d6502059468166bf0bf2
Kumar Chandresh <kumarchandreshsingh@gmail.com>
2025-08-30 20:18:34 +0530

    Add glog command

--- home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
+++ home/Documents/PowerShell/Microsoft.PowerShell_profile.ps1
@@ -4,8 +4,23 @@ Import-Module -Force 'Utils'
 
 Restore-EnvPath # Change "Path" precedence
 
-function which {
-  scoop which @args
+function which ($name) {
+  $path = (
+    Get-Command $name -CommandType Application, ExternalScript -ErrorAction SilentlyContinue |
+    Select-Object -First 1 -ExpandProperty Source
+  ) -replace [regex]::Escape($HOME), '~'
+  if ($path -match '\\scoop\\shims') {
+    $path = & scoop which $name
+  }
+  $path
+}
+
+function glog($n = 10) {
+  git log `
+    --max-count=$n `
+    --color=always `
+    --date=short `
+    --format='%C(yellow)%h %C(red)%ad %C(blue)%an%C(green)%d %C(reset)%s'
 }
 
 Invoke-Expression (& { (oh-my-posh init pwsh --config "$HOME\.config\oh-my-posh\pure.omp.yaml" | Out-String) })
