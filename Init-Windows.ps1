b2e614bb663c79b970b2e5d7ce09457034a8547d
Kumar Chandresh <kumarchandreshsingh@gmail.com>
2025-08-31 16:11:57 +0530

    Refactor: 2025-08-31

--- Init-Windows.ps1
+++ Init-Windows.ps1
@@ -112,42 +112,6 @@ Write-Title '(+) Install Bitwarden CLI'
 Install-ScoopPackage 'main/bitwarden-cli'
 Unlock-Bitwarden
 
-# https://www.chezmoi.io
-Write-Title '(+) Install chezmoi'
-Install-ScoopPackage 'main/chezmoi'
-
-try {
-  chezmoi git status 2>&1 | Out-Null
-}
-finally {
-  Write-Host 'Applying chezmoi changes...' -ForegroundColor Yellow
-  if ($LASTEXITCODE -ne 0) {
-    chezmoi init --apply 'github.com/kumarchandresh' --force
-  }
-  else {
-    chezmoi update --force
-  }
-  if ($LASTEXITCODE -eq 0) {
-    Write-Host 'Done.' -ForegroundColor Green
-  }
-}
-
-$sshGitHub = & ssh -T git@github.com 2>&1 | Out-String
-if ($sshGitHub -match 'kumarchandresh') {
-  if (-not (Test-Path "$HOME\scoop\buckets\private")) {
-    Write-Title "(+) Add scoop bucket: private"
-    & scoop bucket add 'private' 'git@github.com:kumarchandresh/scoop-private.git'
-  }
-
-  # https://www.monolisa.dev
-  Write-Title '(+) Install font: MonoLisa'
-  Install-ScoopPackage 'private/MonoLisa'
-}
-
-# https://github.com/0xType/0xProto
-Write-Title '(+) Install font: 0xProto'
-Install-ScoopPackage 'fonts/0xProtoNerdFont'
-
 # https://wixtoolset.org
 Write-Title '(+) Install dark (WiX Toolset Decompiler)'
 Install-ScoopPackage 'main/dark'
@@ -167,18 +131,6 @@ Install-ScoopPackage 'java/microsoft-lts-jdk'
 
 Write-Title '(+) Install Groovy'
 Install-ScoopPackage 'main/groovy'
-
-# https://github.com/dahlbyk/posh-git
-Write-Title '(+) Install posh-git'
-Install-ScoopPackage 'extras/posh-git'
-
-# https://github.com/devblackops/Terminal-Icons
-Write-Title '(+) Install Terminal-Icons'
-Install-ScoopPackage 'extras/terminal-icons'
-
-# https://ohmyposh.dev
-Write-Title '(+) Install Oh My Posh'
-Install-WinGetPackage 'JanDeDobbeleer.OhMyPosh'
 
 # https://github.com/fastfetch-cli/fastfetch
 Write-Title '(+) Install fastfetch'
@@ -227,6 +179,54 @@ Install-ScoopPackage 'main/tlrc'
 # https://code.visualstudio.com
 Write-Title '(+) Install Visual Studio Code'
 Install-WinGetPackage 'Microsoft.VisualStudioCode' -Config 'vscode.inf'
+
+# https://github.com/dahlbyk/posh-git
+Write-Title '(+) Install posh-git'
+Install-ScoopPackage 'extras/posh-git'
+
+# https://github.com/devblackops/Terminal-Icons
+Write-Title '(+) Install Terminal-Icons'
+Install-ScoopPackage 'extras/terminal-icons'
+
+# https://ohmyposh.dev
+Write-Title '(+) Install Oh My Posh'
+Install-WinGetPackage 'JanDeDobbeleer.OhMyPosh'
+
+# https://www.chezmoi.io
+Write-Title '(+) Install chezmoi'
+Install-ScoopPackage 'main/chezmoi'
+
+try {
+  chezmoi git status 2>&1 | Out-Null
+}
+finally {
+  Write-Host 'Applying chezmoi changes...' -ForegroundColor Yellow
+  if ($LASTEXITCODE -ne 0) {
+    chezmoi init --apply 'github.com/kumarchandresh' --force
+  }
+  else {
+    chezmoi update --force
+  }
+  if ($LASTEXITCODE -eq 0) {
+    Write-Host 'Done.' -ForegroundColor Green
+  }
+}
+
+$sshGitHub = & ssh -T git@github.com 2>&1 | Out-String
+if ($sshGitHub -match 'kumarchandresh') {
+  if (-not (Test-Path "$HOME\scoop\buckets\private")) {
+    Write-Title "(+) Add scoop bucket: private"
+    & scoop bucket add 'private' 'git@github.com:kumarchandresh/scoop-private.git'
+  }
+
+  # https://www.monolisa.dev
+  Write-Title '(+) Install font: MonoLisa'
+  Install-ScoopPackage 'private/MonoLisa'
+}
+
+# https://github.com/0xType/0xProto
+Write-Title '(+) Install font: 0xProto'
+Install-ScoopPackage 'fonts/0xProtoNerdFont'
 
 # TODO: Can we handle this better via chezmoi?
 Write-Title '(+) Install Windows Terminal themes'
